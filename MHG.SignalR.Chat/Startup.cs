6362cb3f20a8d6e8a315b88905a6fdc1ac1de07b
Murat <muhackgames@gmail.com>
2016-11-07 18:07:09 +0300

    First commit

---
+++ MHG.SignalR.Chat/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Owin;
+
+namespace MHG.SignalR.Chat
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
